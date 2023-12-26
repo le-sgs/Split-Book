54ddf58f7e2af6e64f4cda0fd4aae7081f9647db
le-sgs <sgs627@gmail.com>
2023-12-26 13:01:57 +0530

    Changes to handling

--- trainsplit.py
+++ trainsplit.py
@@ -3,11 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-def scrape_train_data(boarding_station, destination_station):
-    # Use a try-except block to handle exceptions
+def scrape_train_data(train_number, boarding_station_code, journey_date):
     try:
         # No need to specify the geckodriver_path when the path to geckodriver is in the system's PATH
         driver = webdriver.Firefox()
@@ -15,35 +13,26 @@ def scrape_train_data(boarding_station, destination_station):
         # Navigate to the IRCTC website
         driver.get("https://www.irctc.co.in/online-charts/")
 
-        # Find the input field by its name
-        input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "react-select-4-input")))
-        input_field.send_keys("12081")
-        input_field.send_keys(Keys.RETURN)
+        # Fill in the train number
+        input_train_number = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "react-select-4-input")))
+        input_train_number.send_keys(train_number)
+        input_train_number.send_keys(Keys.RETURN)
         time.sleep(2)
 
-        # Replace with the actual locator for your date picker input
-        date_picker = driver.find_element(By.XPATH, "//input[@type='text' and @readonly]")
-        driver.execute_script("arguments[0].value = '26-11-23';", date_picker)
+        # Fill in the journey date
+        input_date_picker = driver.find_element(By.XPATH, "//input[@type='text' and @readonly]")
+        driver.execute_script(f"arguments[0].value = '{journey_date}';", input_date_picker)
         time.sleep(1)
 
-
-        input_field2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "react-select-5-input")))
-        input_field2.send_keys("CLT")
-        input_field2.send_keys(Keys.RETURN)
+        # Fill in the boarding station
+        input_boarding_station = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "react-select-5-input")))
+        input_boarding_station.send_keys(boarding_station_code)
+        input_boarding_station.send_keys(Keys.RETURN)
         time.sleep(2)
 
-
-        # Use XPath to find the element by its text content
-        xpath_expression = "//button[text()='GET TRAIN CHART']"
-        get_train_chart_button = driver.find_element(By.XPATH, xpath_expression)
-
-        # Perform a click on the element
-        get_train_chart_button.click()
-        time.sleep(2)
-
-
-        # Print the value of the input field (optional, for verification)
-        print("Input Field Value:", input_field.get_attribute("value"))
+        # Click the "Get Train Chart" button
+        element_text = "Get Train Chart"
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//span[text()='{element_text}']"))).click()
 
     except Exception as e:
         # Handle exceptions, you might want to log the exception details
@@ -55,4 +44,4 @@ def scrape_train_data(boarding_station, destination_station):
             driver.quit()
 
 # Example usage
-scrape_train_data("BoardingStation", "DestinationStation")
+scrape_train_data("12081", "CLT", "26-12-2023")
